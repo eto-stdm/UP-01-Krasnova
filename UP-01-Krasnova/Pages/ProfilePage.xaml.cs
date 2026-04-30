d8a896f0c46c778a056f5c724ae3ddb380f8aaa5
eto-stdm <potatocuteuwu@gmail.com>
2026-04-30 12:13:50 +0300

    gui profile частично

--- UP-01-Krasnova/Pages/ProfilePage.xaml.cs
+++ UP-01-Krasnova/Pages/ProfilePage.xaml.cs
@@ -10,8 +10,10 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UP_01_Krasnova.Classes;
 
 namespace UP_01_Krasnova.Pages
 {
@@ -23,6 +25,17 @@ namespace UP_01_Krasnova.Pages
         public ProfilePage()
         {
             InitializeComponent();
+
+            User cur = Core.Context.User.First(x => x.UserID == State.CurrentUserID);
+            LoginTB.Text += cur.Login;
+            UsernameTB.Text += cur.Username;
+            EmailTB.Text += cur.Email;
+            RoleTB.Text += cur.Role.Name;
+
+            List<Review> userReviews = Core.Context.Review.ToList().FindAll(x => x.UserID == State.CurrentUserID);
+            ReviewsLB.ItemsSource = userReviews;
+
+            //UnfreezeAccTB.Text +=
         }
     }
 }
