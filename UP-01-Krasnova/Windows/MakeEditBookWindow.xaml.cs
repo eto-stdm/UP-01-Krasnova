fd2d34559d7007e05682d10c904785b6db121dcc
eto-stdm <potatocuteuwu@gmail.com>
2026-05-12 20:55:42 +0300

    набросок диалога MakeEditBook

--- UP-01-Krasnova/Windows/MakeEditBookWindow.xaml.cs
+++ UP-01-Krasnova/Windows/MakeEditBookWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using UP_01_Krasnova.Classes;
 
 namespace UP_01_Krasnova.Windows
 {
@@ -25,7 +26,54 @@ namespace UP_01_Krasnova.Windows
             InitializeComponent();
             this.book = book;
 
+            if (book != null)
+            {
+                HeaderTB.Text = "Редактирование книги";
+                NameTB.Text = book.Name;
+                DescriptionTB.Text = book.Description;
+                TextTB.Text = book.Body;
+                CoverTB.Text = book.Cover;
+                MakeEditBookBtn.Content = "Применить изменения";
+            }
+        }
 
+        private void MakeBook()
+        {
+            book = new Book
+            {
+                Name = NameTB.Text,
+                Description = DescriptionTB.Text,
+                Cover = CoverTB.Text,
+                Body = TextTB.Text,
+                AuthorID = State.CurrentUserID,
+                IsFrozen = false,
+            };
+            Core.Context.Book.Add(book);
+            Core.Context.SaveChanges();
+        }
+
+        private void EditBook()
+        {
+            book.Name = NameTB.Text;
+            book.Description = DescriptionTB.Text;
+            book.Body = TextTB.Text;
+            book.Cover = CoverTB.Text;
+            Core.Context.SaveChanges();
+        }
+
+        private void MakeEditBookBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(NameTB.Text) || string.IsNullOrEmpty(DescriptionTB.Text) ||
+                string.IsNullOrEmpty(TextTB.Text) || string.IsNullOrEmpty(CoverTB.Text))
+            {
+                MessageBox.Show("Заполните все поля!");
+            }
+            else
+            {
+                if (book == null) { MakeBook(); }
+                else { EditBook(); }
+                this.DialogResult = true;
+            }
         }
     }
 }
